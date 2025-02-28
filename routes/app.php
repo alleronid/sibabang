0c7f9b1a915a0aedcc124b5be47bbd64fc2b6949
alief <alief.bagas@pinjamyuk.co.id>
2025-02-28 18:05:20 +0700

    add dashboard admin back office

--- routes/app.php
+++ routes/app.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\MerchantController;
+use App\Http\Controllers\WalletController;
 use App\Models\Merchant;
 
 Route::group(['middleware' => 'auth', 'prefix' => 'app'], function () {
@@ -13,7 +14,12 @@ Route::group(['middleware' => 'auth', 'prefix' => 'app'], function () {
         Route::get('/', [MerchantController::class, 'index'])->name('index');
         Route::get('/create', [MerchantController::class, 'create'])->name('create');
         Route::post('/save', [MerchantController::class, 'store'])->name('save');
-        Route::post('/get-merchant', [MerchantController::class, 'getMerchant'])->name('getMerchant');
+        Route::post('/update', [MerchantController::class, 'update'])->name('update');
+        Route::get('/show/{id}', [MerchantController::class, 'getMerchant'])->name('getMerchant');
+    });
+
+    Route::group(['prefix' => 'wallet', 'as' => 'wallet.'], function(){
+        Route::get('/', [WalletController::class, 'index'])->name('index');
     });
 
 });
