107844df762ee14b4181ad2e7d8b942d504ebd74
alief <alief.bagas@pinjamyuk.co.id>
2025-02-28 15:18:20 +0700

    add master vendor

---
+++ database/seeders/VendorSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class VendorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('mt_vendors')->insert([
+            'vendor_name' => 'ayolinx'
+        ]);
+    }
+}
