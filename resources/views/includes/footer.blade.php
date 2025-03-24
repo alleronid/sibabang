e6e47138ab5adce0781b004cbdedd7b2f23a421f
alief <alief.bagas@pinjamyuk.co.id>
2025-03-24 14:34:23 +0700

    change footer name

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -57,7 +57,7 @@
         <!--begin::Copyright-->
         <div class="text-dark order-2 order-md-1">
             <span class="text-muted font-weight-bold mr-2">2022©</span>
-            <a href="#" target="_blank" class="text-dark-75 text-hover-primary">Alief Bagaskara Alamsyah</a>
+            <a href="#" target="_blank" class="text-dark-75 text-hover-primary">SIBABANG KU</a>
         </div>
         <!--end::Copyright-->
         <!--begin::Nav-->
